8c0b1e90b78c9fd7428e5cf41d7da711c9274d5a
Olivier Girardot <olive_girardot@hotmail.com>
2021-10-15 17:02:07 +0200

    The program is now working, just needs a couple of improvements

--- index.js
+++ index.js
@@ -19,51 +19,62 @@ app.listen(3000, () => {
 
   // STEP 1: REMOVE whitespace & '-' from track titles and move the files from
   // the "originalFiles" folder to the "files" folder
-  const replaceWhite = file => {
-    const newFile = file.replace(/(\s-\s|\s|-)/g, '_');
-    return newFile;
-  };
 
-  fs.readdir('./originalFiles', (err, files) => {
-    files.forEach(file => {
-      fs.rename(`./originalFiles/${file}`, `./files/${replaceWhite(file)}`, function(err) {
-          if ( err ) console.log('ERROR: ' + err);
-      });
-    });
-  });
-  
-  // STEP 2: get metadata from files in the "files" folder
-  fs.readdir('./files', function (err, files) {
-    if (err) {
-      console.error("Could not list the directory.", err);
-      process.exit(1);
+  const replace = () => {
+    const replaceWhite = file => {
+      const newFile = file.replace(/(\s-\s|\s|-)/g, '_');
+      return newFile;
     };
-    files.splice(0, 1);
-
-
-    fs.readFile('tracks.json', 'utf8', function readFileCallback(err, data){
-      if (err){
-          console.log(err);
-      } else {
-      const obj = JSON.parse(data); //converts your JSON file into an object
   
-      files.forEach(audio_file => {
-        fileMetadata(`./files/${audio_file}`)
-        .then(res => {
-          const file = {
-            name: res.fsName,
-            bitRate: res.bitsPerSample,
-            sampleRate: res.audioSampleRate
-          };
-          obj.list.push(file); //add some data
-          const json = JSON.stringify(obj); //convert it back to json
-          fs.writeFile('tracks.json', json, 'utf8', (err) => {if (err) throw err }); // write the file back to folder 
+    fs.readdir('./originalFiles', (err, files) => {
+      files.forEach(file => {
+        fs.rename(`./originalFiles/${file}`, `./files/${replaceWhite(file)}`, function(err) {
+            if ( err ) console.log('ERROR: ' + err);
         });
       });
-    }});
-  });
+    });
+  };
+  
+  // STEP 2: get metadata from files in the "files" folder
+  const metadata = () => {
+    fs.readdir('./files', function (err, files) {
+      if (err) {
+        console.error("Could not list the directory.", err);
+        process.exit(1);
+      };
+      files.splice(0, 1);
+  
+  
+      fs.readFile('tracks.json', 'utf8', function readFileCallback(err, data){
+        if (err){
+            console.log(err);
+        } else {
+        const obj = JSON.parse(data); //converts your JSON file into an object
+    
+        files.forEach(audio_file => {
+          fileMetadata(`./files/${audio_file}`)
+          .then(res => {
+            const file = {
+              name: res.fsName,
+              bitRate: res.bitsPerSample,
+              sampleRate: res.audioSampleRate
+            };
+            obj.list.push(file); //add some data
+            const json = JSON.stringify(obj); //convert it back to json
+            fs.writeFile('tracks.json', json, 'utf8', (err) => {if (err) throw err }); // write the file back to folder 
+          });
+        });
+      }});
+    });
+  };
 
-  // STEP 3 (optional): verify if there is the correct amount of files in the JSON file
+  // STEP 3: run both functions asynchronously
+  replace();
+  setTimeout(() => {
+    metadata();
+  }, 5000)
+
+  // STEP 4 (optional): verify if there is the correct amount of files in the JSON file
   // setTimeout(()=> {
   //   fs.readFile('tracks.json', 'utf8', function readFileCallback(err, data){
   //     if (err){
